57a9abaf85e68df9a1bdab1ff391a67d8d638ff1
Ron Winchester <rom0203@gmail.com>
2022-11-27 16:50:51 +0300

    feat: добавил поддержку свайпов на мобильных устройствах

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,6 +7,7 @@ import GameOverlay from "./GameOverlay";
 
 const BoardView = () => {
   const [board, setBoard] = React.useState(new Board());
+  let startX, startY;
   
   const handleKeyDown = (event) => {
     if (board.hasWon()) {
@@ -24,7 +25,44 @@ const BoardView = () => {
     }
   };
 
+  const handleTouchStart = (event) =>  {
+    if (board.hasWon()) {
+      return;
+    }
+    if (event.touches.length !== 1) {
+      return;
+    }
+    startX = event.touches[0].screenX;
+    startY = event.touches[0].screenY;
+    event.isDefaultPrevented();
+  }
+  const handleTouchEnd = (event) => {
+    if (board.hasWon()) {
+      return;
+    }
+    if (event.changedTouches.length !== 1) {
+      return;
+    }
+    var deltaX = event.changedTouches[0].screenX - startX;
+    var deltaY = event.changedTouches[0].screenY - startY;
+    var direction = -1;
+    if (Math.abs(deltaX) > 3 * Math.abs(deltaY) && Math.abs(deltaX) > 30) {
+      direction = deltaX > 0 ? 2 : 0;
+    } else if (Math.abs(deltaY) > 3 * Math.abs(deltaX) && Math.abs(deltaY) > 30) {
+      direction = deltaY > 0 ? 3 : 1;
+    }
+    if (direction !== -1) {
+      let boardClone = Object.assign(
+        Object.create(Object.getPrototypeOf(board)),
+        board
+      );
+      let newBoard = boardClone.move(direction);
+      setBoard(newBoard);
+    }
+  }
+
   useEvent('keydown', handleKeyDown)
+
 
   const cells = board.cells.map((row, rowIndex) => {
     return (
@@ -55,7 +93,7 @@ const BoardView = () => {
           <div>{board.score}</div>
         </div>
       </div>
-      <div className="board">
+      <div className="board"  tabIndex="1" onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd}>
         {cells}
         {tiles}
         <GameOverlay onRestart={resetGame} board={board} />
