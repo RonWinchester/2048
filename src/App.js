1f971248b69c51bd07fc56c5c8c728060d4e1183
Ron Winchester <rom0203@gmail.com>
2022-11-27 20:13:18 +0300

    feat: добавил копирайт

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import "./styles.scss";
 
 function App() {
   return (
-    <BoardView />
+    <div className="app">
+      <BoardView />
+      <footer className="footer">
+        <p className="footer__elements">Design by <a href="https://www.behance.net/romaincousin" target="_balnk">Romain Cousin</a></p>
+        <p className="footer__elements">Algorithm by <a href="https://github.com/IvanVergiliev/2048-react" target="_balnk">Ivan Vergiliev</a></p>
+      </footer>
+    </div>
   );
 }
 
